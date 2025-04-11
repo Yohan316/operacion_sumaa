a2f7d0146e3b62853a72877cb965a084ca9581df
LAB_J205 <labj205@continental.edu.pe>
2025-04-11 13:17:35 -0500

    Se implemento la division

--- test_suma.py
+++ test_suma.py
@@ -1,8 +1,83 @@
 import unittest
+from operacionesaritmeticas import OperacionesAritmeticas
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+
+class TestSuma(unittest.TestCase):
+    def test_suma_dosNumeeros_retornaSuma(self):
+        # Arrange
+        operando1 = 10
+        operando2 = 15
+
+        resultadoEsperado = 25
+
+        objSuma = OperacionesAritmeticas(operando1, operando2)
+
+        # Act
+
+        resultadoActual = objSuma.calcularSuma()
+
+        # Assert
+        self.assertEqual(resultadoEsperado,resultadoActual, "Falló la suma")
+
+    def test_suma_operadorNoNumerico_lanzaExcepcion(self):
+        objSuma = OperacionesAritmeticas(3, "a")
+        with self.assertRaises(TypeError):
+                objSuma.calcularSuma()
+
+    def test_division_dosNumeeros_retornaDivision(self):
+        # Arrange
+        dividendo = 10
+        divisor = 15
+
+        resultadoEsperado = 0.666
+
+        objSuma = OperacionesAritmeticas(dividendo, divisor)
+
+        # Act
+
+        resultadoActual = objSuma.calcularDivision()
+
+        # Assert
+        self.assertAlmostEqual(resultadoEsperado,resultadoActual, 2 ,"Falló la división")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+import unittest
+from division import Division
+
+class TestDivision(unittest.TestCase):
+    def test_division_dosNumeros_retornaResultado(self):
+        # Arrange
+        dividendo = 20
+        divisor = 4
+        resultadoEsperado = 5
+
+        objDivision = Division(dividendo, divisor)
+
+        # Act
+        resultadoActual = objDivision.calcularDivision()
+
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual, "Falló la división")
+
+    def test_division_porCero_lanzaExcepcion(self):
+        # Arrange
+        objDivision = Division(10, 0)
+
+        # Assert
+        with self.assertRaises(ZeroDivisionError):
+            objDivision.calcularDivision()
+
+    def test_division_operadorNoNumerico_lanzaExcepcion(self):
+        # Arrange
+        objDivision = Division(10, "a")
+
+        # Assert
+        with self.assertRaises(TypeError):
+            objDivision.calcularDivision()
 
 if __name__ == '__main__':
     unittest.main()
